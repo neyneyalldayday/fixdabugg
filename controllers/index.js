7b0d71c48e5d255ec70c692c9cae91a5de83e134
neyneyalldayday <drfrankenstein123@gmail.com>
2023-04-05 16:55:56 -0500

    woof

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,8 @@ const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 
-router.use('/api', apiRoutes);
+
 router.use('/', homeRoutes)
+router.use('/api', apiRoutes);
 
 module.exports = router;
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,6 +1,6 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
-
+const { User } = require('../models');
+const { Post } = require("../models")
 const userData = require('./userData.json');
 const projectData = require('./projectData.json');
 
@@ -12,10 +12,10 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
+  for (const posts of projectData) {
+    await Post.create({
+      ...posts,
+      userId: users[Math.floor(Math.random() * users.length)].id,
     });
   }
 
