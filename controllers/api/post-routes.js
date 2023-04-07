a8ec03f1448ceee0823e30e932271d5cc5bbb207
neyneyalldayday <drfrankenstein123@gmail.com>
2023-04-07 17:27:53 -0500

    changed some stuff

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -17,13 +17,13 @@ router.post('/', withAuth, async (req, res) => {
 
   router.put('/:id', withAuth, async (req,res) => {
     try {
-     const [affectedRows] = await Post.update(req.body, {
+     const [updatedStuff] = await Post.update(req.body, {
       where: {
         id: req.params.id,
       },
      });
 
-     if (affectedRows > 0){
+     if (updatedStuff > 0){
       res.status(200).end();
      }else{
       res.status(404).json(err);
