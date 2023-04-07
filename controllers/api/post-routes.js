d7b8a151a46410d5a7cc29c2b313089c6ec713fb
neyneyalldayday <drfrankenstein123@gmail.com>
2023-04-07 15:07:10 -0500

    comment route and view

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -15,5 +15,28 @@ router.post('/', withAuth, async (req, res) => {
 
 
 
+  router.put('/:id', withAuth, async (req,res) => {
+    try {
+     const [affectedRows] = await Post.update(req.body, {
+      where: {
+        id: req.params.id,
+      },
+     });
+
+     if (affectedRows > 0){
+      res.status(200).end();
+     }else{
+      res.status(404).json(err);
+     }
+    } catch (err) {
+      res.status(500).json(err)
+    }
+  });
+
+
   
+
+
+
+
 module.exports = router;
